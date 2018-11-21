cf41f3274e33ebef97460baad128c4878fddd192
Jonathan Somer <jonathan.somer@fiverr.com>
2018-11-21 23:37:10 +0200

    connects to mongo

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from logging.config import dictConfig
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://soroka-hackathon:a123456@ds247178.mlab.com:47178/heroku_6q3wqvtk"
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	an_incrementing_number = mongo.db.number.find_one_and_update({ "_id" : "the only id" }, {"$inc" : { "number" : 1 }}, upsert=True)['number']
+	return "A number which increments upon visiting this site: {}".format(an_incrementing_number)
