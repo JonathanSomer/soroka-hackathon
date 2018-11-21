853d79dbef458a2787c36fd7811486fffdc76e6b
Jonathan Somer <jonathan.somer@fiverr.com>
2018-11-22 00:19:15 +0200

    hoppaaa hackathon

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 from logging.config import dictConfig
 
@@ -9,4 +9,4 @@ mongo = PyMongo(app)
 @app.route('/')
 def hello_world():
 	an_incrementing_number = mongo.db.number.find_one_and_update({ "_id" : "the only id" }, {"$inc" : { "number" : 1 }}, upsert=True)['number']
-	return "A number which increments upon visiting this site: {}".format(an_incrementing_number)
+	return render_template('home.html', number=an_incrementing_number)
