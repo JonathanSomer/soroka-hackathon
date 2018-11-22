2d68fd9fcef5762be7461f9c9cadc07526475e71
kessido@gmail.com <kessido@gmail.com>
2018-11-22 23:06:45 +0200

    builded

--- client/dist/client/polyfills.js
+++ client/dist/client/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Volumes/dev/soroka-hackathon/client/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\kessi\Repo\soroka-hackathon\client\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
